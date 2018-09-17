847bcad45a01d136aeb04a5411a86c1ba6d48c0d
Aakash Sahu <coolakss@yahoo.com>
2018-09-17 13:20:37 -0400

    updated

--- Package/flaskblog/__init__.py
+++ Package/flaskblog/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+#login library
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '52e943135217b09a69608306d2fa4865'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+#initializing login manager and functioanlity added in models to validate login
+login_manager = LoginManager(app)
 
 from flaskblog import routes
 
--- Package/flaskblog/models.py
+++ Package/flaskblog/models.py
@@ -1,7 +1,20 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db,login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+""" 
+add decorator function to get the user id 
+function expects user module to have certain attributes and method. thats' why we inherit from 
+another class. is_active, get_id, is_authenticated. 
+extension provides that class, UserMixin
+Next modify login route
+"""
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id)
+
+class User(db.Model, UserMixin): 
 	id = db.Column(db.Integer, primary_key = True)
 	username = db.Column(db.String(20), unique = True, nullable = False)
 	email = db.Column(db.String(120), unique = True, nullable = False)
